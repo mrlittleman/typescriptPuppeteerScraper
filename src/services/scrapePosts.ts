73cb6f0ffe050b921cb9d5912715ae145165f6db
Engr-Nyx <arcee.tomaquin@gmail.com>
2025-06-10 20:10:32 +0800

    test

--- src/services/scrapePosts.ts
+++ src/services/scrapePosts.ts
@@ -2,13 +2,13 @@ import { scrapePosts } from '../services/scraper';
 import { KEYWORDS } from '../constants/keywords';
 
 export async function scrapeAndSave(): Promise<void> {
-  console.log(process.env.FB_GROUP);
-  if (!process.env.FB_GROUP) {
+  const group = 'tfwmanilaqc';
+  if (!group) {
     throw new Error('FB_GROUP environment variable not set');
   }
 
   await scrapePosts({
     keywords: KEYWORDS,
-    groupId: process.env.FB_GROUP
+    groupId: group
   });
 }
